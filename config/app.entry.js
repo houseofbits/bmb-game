29961b344668a8ef850d0d74d59c5228e015c691
Krists Pudzens <kpudzens@gmail.com>
2023-09-09 13:41:30 +0300

    a

--- config/app.entry.js
+++ config/app.entry.js
@@ -1,12 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: {
-        'main': './app/src/main.ts',
-    },
-    output: {
-        filename: '[name].js',
-        publicPath: '/',
-        path: path.resolve(__dirname, '../dist/'),
-    },
+  entry: {
+    main: "./app/src/main.ts",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    publicPath: "/",
+    path: path.resolve(__dirname, "../dist/"),
+  },
 };
--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -3,12 +3,14 @@ const { VueLoaderPlugin } = require("vue-loader");
 const { SourceMapDevToolPlugin } = require("webpack");
 const webpack = require("webpack");
 const { VuetifyPlugin } = require("webpack-plugin-vuetify");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const packageVersion = require("../package.json").version;
 
 module.exports = {
   performance: { hints: false },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: "[name].[contenthash].css",
       chunkFilename: "[id].css",
     }),
     new VueLoaderPlugin(),
@@ -22,6 +24,24 @@ module.exports = {
     new VuetifyPlugin({
       autoImport: true,
       // styles: { configFile: "app/style/variables.scss" },
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      templateContent: ({ htmlWebpackPlugin }) => `
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>BMBGAME ${packageVersion}</title>      
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"/>
+        ${htmlWebpackPlugin.tags.headTags}
+      </head>
+        <body oncontextmenu="return false;">
+        <div id="gui"></div>
+        ${htmlWebpackPlugin.tags.bodyTags}
+      </body>
+    </html>
+  `,
     }),
   ],
 };
