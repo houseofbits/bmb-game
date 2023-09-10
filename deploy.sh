e12e8d18909e50f26e7d03b6aa005cc94fc1d289
Krists Pudzens <kpudzens@gmail.com>
2023-09-10 16:31:25 +0300

    a

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 git add -A
 git commit -am"a"
 rm dist/*
+git add -A
+git commit -am"a"
 npm run prod
 ssh dizainsu@dizainsuninterjers.lv "rm bmb.dizainsuninterjers.lv/*"
 scp -r dist/* dizainsu@dizainsuninterjers.lv:bmb.dizainsuninterjers.lv/
