008980421d88527650746482261d73dd8579a547
Krists Pudzens <kpudzens@gmail.com>
2023-09-16 17:11:21 +0300

    a

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
+echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Committing changes${ENDCOLOR}"
 git add -A
 git commit -am"a"
 
+echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Cleaning up /dist folder${ENDCOLOR}"
 rm dist/*
 git add -A
 git commit -am"a"
 
+echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Running npm build${ENDCOLOR}"
 npm run prod
 
+echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Cleaning up remote server${ENDCOLOR}"
 ssh dizainsu@dizainsuninterjers.lv "rm bmb.dizainsuninterjers.lv/*"
 
+echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Copying files to remote server${ENDCOLOR}"
 scp -r dist/* dizainsu@dizainsuninterjers.lv:bmb.dizainsuninterjers.lv/
