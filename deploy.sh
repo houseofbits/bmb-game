cdf22cce1274141b5e94f32197269f0f32c8a7cd
Krists Pudzens <kpudzens@gmail.com>
2023-09-16 17:13:01 +0300

    a

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,22 @@ ENDCOLOR="\e[0m"
 
 echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Committing changes${ENDCOLOR}"
-git add -A
-git commit -am"a"
+git add -A > /dev/null 2>&1
+git commit -am"a" > /dev/null 2>&1
 
 echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Cleaning up /dist folder${ENDCOLOR}"
-rm dist/*
-git add -A
-git commit -am"a"
+rm dist/* > /dev/null 2>&1
+git add -A > /dev/null 2>&1
+git commit -am"a" > /dev/null 2>&1
 
 echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Running npm build${ENDCOLOR}"
-npm run prod
+npm run prod > /dev/null 2>&1
 
 echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Cleaning up remote server${ENDCOLOR}"
-ssh dizainsu@dizainsuninterjers.lv "rm bmb.dizainsuninterjers.lv/*"
+ssh dizainsu@dizainsuninterjers.lv "rm bmb.dizainsuninterjers.lv/*" > /dev/null 2>&1
 
 echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Copying files to remote server${ENDCOLOR}"
