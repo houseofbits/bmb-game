6493116ae5d5e5b69d251b98062f770e6966b06b
Krists Pudzens <kpudzens@gmail.com>
2023-09-16 17:25:49 +0300

    a

--- app/src/config/devicePresets.ts
+++ app/src/config/devicePresets.ts
@@ -52,7 +52,7 @@ export default [
       "simonSays",
       "wireMatcher",
       "morseCode",
-      "binaryLogic",
+      "binaryLogic2",
       "labyrinth",
       "numberDecoder",
     ],
--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,22 @@
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
-echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
-echo -e "${GREEN}Committing changes${ENDCOLOR}"
+echo -e "${GREEN}Committing changes to git${ENDCOLOR}"
 git add -A > /dev/null 2>&1
 git commit -am"a" > /dev/null 2>&1
 
-echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Cleaning up /dist folder${ENDCOLOR}"
 rm dist/* > /dev/null 2>&1
 git add -A > /dev/null 2>&1
 git commit -am"a" > /dev/null 2>&1
 
-echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Running npm build${ENDCOLOR}"
 npm run prod > /dev/null 2>&1
 
-echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Cleaning up remote server${ENDCOLOR}"
 ssh dizainsu@dizainsuninterjers.lv "rm bmb.dizainsuninterjers.lv/*" > /dev/null 2>&1
 
-echo -e "${GREEN}--------------------------------------------------------${ENDCOLOR}"
 echo -e "${GREEN}Copying files to remote server${ENDCOLOR}"
 scp -r dist/* dizainsu@dizainsuninterjers.lv:bmb.dizainsuninterjers.lv/
+
+echo -e "${GREEN}DONE!${ENDCOLOR}"
